ec23f034082172d585276188948180b17eac08d3
Santiago Enol <santieherrero@gmail.com>
2012-12-04 18:35:17 +0100

    Terminado subcripcion,formulario,flash

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "User Pages" do
 	describe "sign up page" do
 		
 		before{ visit sign_up_path }
-
+ 
 		it { should have_selector('h1',   text: 'Sign up')}	
 		it { should have_selector('title',text: title_bar('Sign up'))}
 
@@ -22,4 +22,43 @@ describe "User Pages" do
 		it { should have_selector('title',text: user.name)}
 
 	end
+
+
+	describe "Sign up" do
+
+		before { visit sign_up_path }
+
+		let(:submit) { "Create my account" }
+
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User,:count)
+			end
+		end
+
+		describe "with valid information" do
+
+			before do
+				fill_in "Name", 			with: "Santiago"
+				fill_in "Email", 			with: "xantiago@gmail.com"
+				fill_in "Password", 		with: "wololo"
+				fill_in "Confirmation", 	with: "wololo"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User,:count).by(1)
+			end
+		
+
+			describe "after saving a user" do
+				
+				before{ click_button submit }
+
+				let(:user) { User.find_by_email("xantiago@gmail.com")}
+				it{ should have_selector('title', text: user.name ) } 
+			end
+
+		end
+	end
+
 end
